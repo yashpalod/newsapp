25f3fd9c6c1cd6e707c30b12a600ab3b645b4d2a
yashpalod <yashpalod01@gmail.com>
2022-01-20 15:43:49 +0530

    add time ,author, source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card">
+                    <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>
+                        {source}
+                    </span>
                     <img src={!imageUrl ? "https://st1.latestly.com/wp-content/uploads/2022/01/9-Watch-784x441.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p className='card-text'><small className="text-muted">By {!author ? "unkonown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
